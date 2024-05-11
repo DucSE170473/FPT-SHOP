7c14df7811827f5909c705dd2bdaa36fa54d7fae
Ho Minh Quyen <minhquyenho09@gmail.com>
2024-05-11 10:42:13 +0700

    Add folder HOC_ASP.NET

---
+++ Hoc_ASP.NET_web/mvc01_HelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvc01_HelloWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
